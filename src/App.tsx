1e415faef401b8e653dade3f46d8386866cb2b55
Antonio-el-Bear <antoniozambuko@gmail.com>
2026-03-19 15:11:56 +0200

    remove speed insight

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Toaster } from '@/components/ui/toaster';
 import { Toaster as Sonner } from '@/components/ui/sonner';
 import { TooltipProvider } from '@/components/ui/tooltip';
-import { SpeedInsights } from "@vercel/speed-insights";
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Index from './pages/Index';
@@ -27,7 +27,6 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <SpeedInsights />
       <Toaster />
       <Sonner />
       <BrowserRouter>
